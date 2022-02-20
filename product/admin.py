3db13f8aa9754c17e27c7f012b25c2098bf5e06b
YoannaLa <joannaelazarek@yahoo.co.uk>
2022-02-20 19:22:57 +0000

    adding product details

--- product/admin.py
+++ product/admin.py
@@ -3,16 +3,6 @@ from .models import Product
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Product)
-class ProductAdmin(SummernoteModelAdmin):
-    list_display = ('name', 'description', 'price')
-    search_fields = ['name', 'description']
-    ordering = ('name',)
-    summernote_fields = ('description',)
-
-
-# @admin.register(Id)
-# class IdAdmin(admin.ModelAdmin):
-#     list_display = ('name',)
+admin.site.register(Product)
 
 
